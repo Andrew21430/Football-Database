539bd0ae7574509852134f31c841db55e67aeff7
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-04-10 09:58:41 -0500

    add more pages with there data printing fix inter

--- program.py
+++ program.py
@@ -50,6 +50,46 @@ def homepage():
 def about():
     return render_template("about.html")
 
+@app.route('/player')
+def player():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor() 
+    # sql statement
+    sql = "SELECT * FROM Player;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    return render_template("player.html", results=results)
+
+@app.route('/club')
+def club():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor() 
+    # sql statement
+    sql = "SELECT * FROM Club;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    return render_template("club.html", results=results)
+
+@app.route('/international')
+def international():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor() 
+    # sql statement
+    sql = "SELECT * FROM International;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    return render_template("international.html")
+
+@app.route('/award')
+def award():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor() 
+    # sql statement
+    sql = "SELECT * FROM Award;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    return render_template("award.html", results=results)
+
 
 
 if __name__ == "__main__":
