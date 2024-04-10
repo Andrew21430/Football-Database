f85bc8be78e282489e52c7cd81a5c5948233d783
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-04-10 10:25:27 -0500

    chandge footer and add new font for navbar

--- program.py
+++ program.py
@@ -13,26 +13,6 @@ DATABASE = "Database.db"
 app = Flask(__name__)
 
 
-'''SQL Functions'''
-
-
-'''Printing'''
-
-
-def print_players():
-    db = sqlite3.connect(DATABASE)
-    cursor = db.cursor() 
-    # sql statement
-    sql = "SELECT * FROM Player;"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    #print format
-    print(f"{'id':<10} {'player':<30} {'club id':<10} {'apperances':<15} {'age':<5} {'club count':<15} {'international id'}")
-    for player in results:
-        #printing out the data
-        print(f"{player[0]: <10} {player[1]:<30} {player[2]:<10} {player[3]:<15} {player[4]:<5} {player[5]:<15} {player[6]} ")
-    db.close()
-
 
 @app.route('/')
 
